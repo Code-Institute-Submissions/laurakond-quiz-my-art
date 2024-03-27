de7e5b9ed3fffcd7bbf9d15847eae6d67ac5d2db
laurakond <laura.kondrataite@gmail.com>
2024-03-27 19:37:20 +0000

    script.js(add test event listener code)

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// const buttonClick = document.querySelector('button');
+// buttonClick.addEventListener('click', artistCategory);
+
+// function artistCategory() {
+//   window.location.href = "artist.html";
+// }
