fbdda71311729be5ec1870222bd2b146aecd230e
laurakond <laura.kondrataite@gmail.com>
2024-03-29 13:56:06 +0000

    js(add list of questions for artist category) assets(add images)

---
+++ assets/js/artist.js
@@ -0,0 +1,65 @@
+/** The following video was used as a source for creating
+ * a list of questions for all categories:
+ * https://www.youtube.com/watch?v=riDzcEQbX6k
+ */
+
+
+// Images sourced from Wikipedia.org
+// Questions for the artist category 
+
+const artistQuestions = [
+ {
+ imageUrl: "assets/images/raphael.webp",
+ answers: [
+  {text: 'Raphael', correct: true},
+  {text: 'Michelangelo', correct: false},
+  {text: 'Boticelli', correct: false},
+  {text: 'Da Vinci', correct: false}
+ ]
+},
+{
+ imageUrl: "assets/images/el-greco.webp",
+ answers: [
+  {text: 'Goya', correct: false},
+  {text: 'David', correct: false},
+  {text: 'Caravaggio', correct: false},
+  {text: 'El Greco', correct: true}
+ ]
+},
+{
+ imageUrl: "assets/images/frida-kahlo.webp",
+ answers: [
+  {text: ' Pablo Picasso', correct: false},
+  {text: 'Frida Kahlo', correct: true},
+  {text: 'Edgar Degas', correct: false},
+  {text: 'Berthe Morrisot', correct: false}
+ ]
+},
+{
+ imageUrl: "assets/images/michelangelo-david.webp",
+ answers: [
+  {text: 'Auguste Rodin', correct: false},
+  {text: 'Michelangelo', correct: true},
+  {text: 'Gian Lorenzo Bernini', correct: false},
+  {text: 'Henry Moore', correct: false}
+ ]
+},
+{
+ imageUrl: "assets/images/mona-lisa.webp",
+ answers: [
+  {text: 'Leonardo Da Vinci', correct: true},
+  {text: 'Titian', correct: false},
+  {text: 'Albrecht Durer', correct: false},
+  {text: 'Raphael', correct: false}
+ ]
+},
+{
+ imageUrl: "assets/images/monet-impression-sunrise.webp",
+ answers: [
+  {text: 'Edouard Manet', correct: true},
+  {text: 'Claude Monet', correct: false},
+  {text: 'Pierre-Auguste Renoir', correct: false},
+  {text: 'Edgar Degas', correct: false}
+ ]
+}
+]
