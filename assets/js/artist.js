30cb0734b21c09b54911da0045147f6cd0667ec3
laurakond <laura.kondrataite@gmail.com>
2024-03-30 08:20:50 +0000

    js(add images for artist category and complete list of answers in artist.js)

--- assets/js/artist.js
+++ assets/js/artist.js
@@ -13,8 +13,8 @@ const artistQuestions = [
  answers: [
   {text: 'Raphael', correct: true},
   {text: 'Michelangelo', correct: false},
-  {text: 'Boticelli', correct: false},
-  {text: 'Da Vinci', correct: false}
+  {text: 'Sandro Botticelli', correct: false},
+  {text: 'Leonardo Da Vinci', correct: false}
  ]
 },
 {
@@ -29,7 +29,7 @@ const artistQuestions = [
 {
  imageUrl: "assets/images/frida-kahlo.webp",
  answers: [
-  {text: ' Pablo Picasso', correct: false},
+  {text: 'Pablo Picasso', correct: false},
   {text: 'Frida Kahlo', correct: true},
   {text: 'Edgar Degas', correct: false},
   {text: 'Berthe Morrisot', correct: false}
@@ -40,7 +40,7 @@ const artistQuestions = [
  answers: [
   {text: 'Auguste Rodin', correct: false},
   {text: 'Michelangelo', correct: true},
-  {text: 'Gian Lorenzo Bernini', correct: false},
+  {text: 'Gian Lorenzo Berninni', correct: false},
   {text: 'Henry Moore', correct: false}
  ]
 },
@@ -60,6 +60,132 @@ const artistQuestions = [
   {text: 'Claude Monet', correct: false},
   {text: 'Pierre-Auguste Renoir', correct: false},
   {text: 'Edgar Degas', correct: false}
+ ]
+},
+{
+ imageUrl: "assets/images/rembrandt-self-portrait.webp",
+ answers: [
+  {text: 'Rubens', correct: false},
+  {text: 'Claude Poussin', correct: false},
+  {text: 'Rembrandt Van Rijn', correct: true},
+  {text: 'Frans Hals', correct: false}
+ ]
+},
+{
+ imageUrl: "assets/images/titian-diana-actaeon.webp",
+ answers: [
+  {text: 'Titian', correct: true},
+  {text: 'Sandro Botticelli', correct: false},
+  {text: 'Giorgio Vasari', correct: false},
+  {text: 'Paolo Uccello', correct: false}
+ ]
+},
+{
+ imageUrl: "assets/images/vangogh-stary-night.webp",
+ answers: [
+  {text: 'Camille Pissarro', correct: false},
+  {text: 'Henri de Toulouse-Lautrec', correct: false},
+  {text: 'Paul Cezanne', correct: false},
+  {text: 'Van Gogh', correct: true}
+ ]
+},
+{
+ imageUrl: "assets/images/vermeer-girl-with-pearl-earring.webp",
+ answers: [
+  {text: 'Vermeer', correct: true},
+  {text: 'Jan Steen', correct: false},
+  {text: 'Gerrit van Honthorst', correct: false},
+  {text: 'Frans Hals', correct: false}
+ ]
+},
+{
+ imageUrl: "assets/images/jackson-pollock.webp",
+ answers: [
+  {text: 'Max Ernst', correct: false},
+  {text: 'Andre Mason', correct: false},
+  {text: 'Jackson Pollock', correct: true},
+  {text: 'Mark Rothko', correct: false}
+ ]
+},
+{
+ imageUrl: "assets/images/grant-wood-american-gothic.webp",
+ answers: [
+  {text: 'George Bellows', correct: false},
+  {text: 'Grant Wood', correct: true},
+  {text: 'Marsden Hartley', correct: false},
+  {text: 'Thomas Hart Benton', correct: false}
+ ]
+},
+{
+ imageUrl: "assets/images/henri-matisse-the-danse.webp",
+ answers: [
+  {text: 'Pablo Picasso', correct: false},
+  {text: 'George Braque', correct: false},
+  {text: 'Henri Matisse', correct: true},
+  {text: 'Paul Cezanne', correct: false}
+ ]
+},
+{
+ imageUrl: "assets/images/paul-cezanne-montagne-sainte-victoire.webp",
+ answers: [
+  {text: 'George Bellows', correct: false},
+  {text: 'Francisco de Goya', correct: false},
+  {text: 'Gustave Courbet', correct: true},
+  {text: 'Paul Cezanne', correct: false}
+ ]
+},
+{
+ imageUrl: "assets/images/auguste-robin-the-thinker.webp",
+ answers: [
+  {text: 'Jeff Koons', correct: false},
+  {text: 'Constantin Brancusi', correct: false},
+  {text: 'Auguste Rodin', correct: true},
+  {text: 'Donatello', correct: false}
+ ]
+},
+{
+ imageUrl: "assets/images/edward-hopper-nighthawks.webp",
+ answers: [
+  {text: 'Edward Hopper', correct: true},
+  {text: 'George Bellows', correct: false},
+  {text: 'Grant Wood', correct: false},
+  {text: 'Thomas Hart Benton', correct: false}
+ ]
+},
+{
+ imageUrl: "assets/images/piet-mondrian-mondrian-red-blue-yellow.webp",
+ answers: [
+  {text: 'Gustav Klimt', correct: false},
+  {text: 'Piet Mondrian', correct: true},
+  {text: 'Van Gogh', correct: false},
+  {text: 'Edgar Degas', correct: false}
+ ]
+},
+{
+ imageUrl: "assets/images/the-kiss-klimt.webp",
+ answers: [
+  {text: 'Gustav Klimt', correct: true},
+  {text: 'Edvard Munch', correct: false},
+  {text: 'Wassily Kandinski', correct: false},
+  {text: 'Egon Schiele', correct: false}
+ ]
+},
+{
+ imageUrl: "assets/images/bernini-the-ecstasy-of-saint-theresa.webp",
+ answers: [
+  {text: 'Auguste Rodin', correct: false},
+  {text: 'Bernini', correct: true},
+  {text: 'Antonio Canova', correct: false},
+  {text: 'Donatello', correct: false}
+ ]
+},
+{
+ imageUrl: "assets/images/rothko-orange-tan-1954.webp",
+ answers: [
+  {text: 'Jackson Pollock', correct: false},
+  {text: 'Arshile Gorky', correct: false},
+  {text: 'Richard Pousette-Dart', correct: false},
+  {text: 'Mark Rothko', correct: true}
  ]
 }
 ]
