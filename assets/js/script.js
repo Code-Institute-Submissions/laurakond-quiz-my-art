339be10f37bb43eaf1ee25e50b06ec10a4775bd6
laurakond <laura.kondrataite@gmail.com>
2024-04-01 16:08:12 +0100

    js(add initial DOM event listener and iterate through category buttons)

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,23 +11,45 @@
  */
 
 
-document.addEventListener("DOMContentLoaded", function () {
- let categoryButtons = document.getElementsByTagName('button');
+// document.addEventListener("DOMContentLoaded", function () {
+//  let categoryButtons = document.getElementsByTagName('button');
 
- for (let button of categoryButtons) {
-  button.addEventListener("click", function () {
-   if (this.getAttribute("data-type") === "artist") {
-    window.location.href = "artist.html";
-   } else if (this.getAttribute("data-type") === "period") {
-    window.location.href = "period.html";
-   } else if (this.getAttribute("data-type") === "painting") {
-    window.location.href = "painting.html";
-   } else {
-    let categoryType = this.getAttribute("bonus");
-    window.location.href = "bonus.html";
-   }
-  })
- }
+//  for (let button of categoryButtons) {
+//   button.addEventListener("click", function () {
+//    if (this.getAttribute("data-type") === "artist") {
+//     window.location.href = "artist.html";
+//    } else if (this.getAttribute("data-type") === "period") {
+//     window.location.href = "period.html";
+//    } else if (this.getAttribute("data-type") === "painting") {
+//     window.location.href = "painting.html";
+//    } else {
+//     let categoryType = this.getAttribute("bonus");
+//     window.location.href = "bonus.html";
+//    }
+//   })
+//  }
+// })
+
+document.addEventListener("DOMContentLoaded", function (){
+    let categoryButtons = document.getElementsByTagName("button");
+
+    for (let button of categoryButtons) {
+        button.addEventListener("click", function(){
+            let categoryType = this.getAttribute("id");
+            if(this.getAttribute("id") === "artist") {
+                console.log("you clicked artist category");
+            } else if (this.getAttribute("id") === "period") {
+                console.log("you clicked period category");
+            } else if (this.getAttribute("id") === "painting") {
+                console.log("you clicked painting category");
+            } else if(this.getAttribute("id") === "bonus") {
+                console.log("you clicked I am an expert category");
+            } else{
+                // let categoryType = this.getAttribute("id");
+                console.log(`you clicked ${categoryType} category`);
+            };
+        })
+    }
 })
 
 /**function for generating random questions in each category */
@@ -50,5 +72,5 @@ function scoreBoard(){}
 /** Calculates the total score of correctly answered questions
  * and prompts appropriate congratulatory(consoling) paragraph
  */
-function totalScore(){}
+function calculateTotalScore(){}
 
