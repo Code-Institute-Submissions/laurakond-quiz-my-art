ccaefd1436bf6b142ce1958dffd8128093797a72
laurakond <laura.kondrataite@gmail.com>
2024-04-10 08:07:45 +0100

    js(map out required functions for the gameplay)

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,10 @@
  * and selects each button to access different category of the game
  */
 
+/** The following video was used as a template and inspiration for creating
+ * a list of questions for all categories and also apply question display in each category:
+ * https://www.youtube.com/watch?v=riDzcEQbX6k
+ */
 
 // document.addEventListener("DOMContentLoaded", function () {
 //  let categoryButtons = document.getElementsByTagName('button');
@@ -30,34 +34,32 @@
 //  }
 // })
 
+// document.addEventListener("DOMContentLoaded", function (){
+//     let categoryButtons = document.getElementsByTagName("button");
 
+//     for (let button of categoryButtons) {
+//         button.addEventListener("click", function(){
+//             let categoryType = this.getAttribute("id");
+//             if(this.getAttribute("id") === "artist") {
+//                 chooseCategory(categoryType);
+//             } else if (this.getAttribute("id") === "period") {
+//                 console.log("you clicked period category");
+//             } else if (this.getAttribute("id") === "painting") {
+//                 console.log("you clicked painting category");
+//             } else if(this.getAttribute("id") === "bonus") {
+//                 console.log("you clicked I am an expert category");
+//             } else{
+//                 // let categoryType = this.getAttribute("id");
+//                 startGame(categoryType);
+//             };
+//         })
+//     }
 
-document.addEventListener("DOMContentLoaded", function (){
-    let categoryButtons = document.getElementsByTagName("button");
-
-    for (let button of categoryButtons) {
-        button.addEventListener("click", function(){
-            let categoryType = this.getAttribute("id");
-            if(this.getAttribute("id") === "artist") {
-                chooseCategory(categoryType);
-            } else if (this.getAttribute("id") === "period") {
-                console.log("you clicked period category");
-            } else if (this.getAttribute("id") === "painting") {
-                console.log("you clicked painting category");
-            } else if(this.getAttribute("id") === "bonus") {
-                console.log("you clicked I am an expert category");
-            } else{
-                // let categoryType = this.getAttribute("id");
-                runGame(categoryType);
-            };
-        })
-    }
-
-    runGame();
-})
+//     runGame();
+// })
 
 /** Initialises category selection to start the game */
-function runGame(categoryType){
+function startGame(categoryType){
     console.log("started");
     chooseCategory();
     
@@ -68,25 +70,58 @@ function runGame(categoryType){
 //     console.log("works");
 // }
 
-/**function for generating random questions in each category */
-function randomQuestion () {}
+/**Function that generates random questions in each category */
+function getRandomQuestion () {}
+
+/** Function that generate a question in the getRandomQuestion function above*/
+function showQuestion() {}
+
+/** Function that populates Question area container based on chosen category
+ * and applies a class name to the correct answer
+ */
+function displayQuestion() {}
+
+/** Function that allows the user to select their answer button */
+function userAnswer(){}
+
+/**Function that generates 10 questions */
+function nextQuestion(){}
+
+/** Function that resets the Question area section for the next question */
+function resetQuestion(){}
+
+/** Function that shows which question the user is at against the total number of questions */
+function incrementQuestionNumber(){}
 
 /** Shows which question the user is at against the total number of questions */
 function currentQuestionNumber(){}
 
-/** Allows the user to inout their own answer in the Bonus category */
-function userAnswer(){}
+/** Function that controls next button */
+function handleNextButtonClick(){}
+
+/** Function that will calculate the score as the quiz progresses. 
+ * But will not display until the end 
+ */
+function incrementScore(){}
+
+/** Function that calculates the total score of correctly answered questions */
+function totalScore(){}
+
+/**Function that generates appropriate text based on total score */
+function displayScoreText(){}
+
+/** Function that opens the Score/final page after the completion of the quiz */
+function scoreBoard(){}
+
+/**Function that resets the game */
+function resetGame(){}
 
 /**
  * Checks the user's answer against the correct answer
  */
-function checkCorrectAnswer(){}
+// function checkCorrectAnswer(){}
 
-/** Triggers the Score page after the completion of each category */
-function scoreBoard(){}
 
-/** Calculates the total score of correctly answered questions
- * and prompts appropriate congratulatory(consoling) paragraph
- */
-function calculateTotalScore(){}
+
+
 
