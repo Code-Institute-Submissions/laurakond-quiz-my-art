452aeebc4a47a9866a8d61f0b82ca06f5a499648
laurakond <laura.kondrataite@gmail.com>
2024-04-11 20:11:17 +0100

    js(move code that initialises categories to separate functions & remove unused code)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,62 +1,7 @@
-// const buttonClick = document.querySelector('button');
-// buttonClick.addEventListener('click', quizCategory);
-
-// function quizCategory() {
-//   window.location.href = "artist.html";
-// }
-
-// This section was inspired by Love Maths walkthrough project by Code Institute.
-/** This part waits for the DOM to load
- * and selects each button to access different category of the game
- */
-
 /** The following video was used as a template and inspiration for creating
  * a list of questions for all categories and also apply question display in each category:
  * https://www.youtube.com/watch?v=riDzcEQbX6k
  */
-
-// document.addEventListener("DOMContentLoaded", function () {
-//  let categoryButtons = document.getElementsByTagName('button');
-
-//  for (let button of categoryButtons) {
-//   button.addEventListener("click", function () {
-//    if (this.getAttribute("data-type") === "artist") {
-//     window.location.href = "artist.html";
-//    } else if (this.getAttribute("data-type") === "period") {
-//     window.location.href = "period.html";
-//    } else if (this.getAttribute("data-type") === "painting") {
-//     window.location.href = "painting.html";
-//    } else {
-//     let categoryType = this.getAttribute("bonus");
-//     window.location.href = "bonus.html";
-//    }
-//   })
-//  }
-// })
-
-// document.addEventListener("DOMContentLoaded", function (){
-//     let categoryButtons = document.getElementsByTagName("button");
-
-//     for (let button of categoryButtons) {
-//         button.addEventListener("click", function(){
-//             let categoryType = this.getAttribute("id");
-//             if(this.getAttribute("id") === "artist") {
-//                 chooseCategory(categoryType);
-//             } else if (this.getAttribute("id") === "period") {
-//                 console.log("you clicked period category");
-//             } else if (this.getAttribute("id") === "painting") {
-//                 console.log("you clicked painting category");
-//             } else if(this.getAttribute("id") === "bonus") {
-//                 console.log("you clicked I am an expert category");
-//             } else{
-//                 // let categoryType = this.getAttribute("id");
-//                 startGame(categoryType);
-//             };
-//         })
-//     }
-
-//     runGame();
-// })
 
 // Static variables used for accessing DOM
 const logo = document.getElementById('logo');
@@ -87,6 +32,7 @@ let questionNumberCount = 0;
 
 // Event Listeners
 nextButton.addEventListener('click', handleNextButtonClick);
+
 // DOM content loaded event listener & each category iteration using a for loop
 document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByTagName("button");
@@ -95,80 +41,36 @@ document.addEventListener("DOMContentLoaded", function () {
         button.addEventListener("click", function () {
             let categoryType = this.getAttribute("id");
             startGame(categoryType);
-            // "this" refers to specific button pressed, ie "Who painted it" button that has id of "artist"
-            // if (this.getAttribute("id") === "artist") {
-            //     console.log(`you clicked ${categoryType} category button`);
-            //     startGame();
-            // } else if (this.getAttribute("id") === "period") {
-            //     console.log(`you clicked ${categoryType} category button`);
-            //     startGame();
-            // } else if (this.getAttribute("id") === "painting") {
-            //     console.log(`you clicked ${categoryType} category button`);
-            //     startGame();
-            // } else {
-            //     console.log(`error`);
-            // }
         });
     }
 });
 
 
 /** Initialises category selection to start the game */
-function startGame(id){
-    console.log("started");
-    // chooseCategory();
+function startGame(id) {
     if (id === "artist") {
-        artistCategoryButton.classList.add('hide');
-        periodCategoryButton.classList.add('hide');
-        paintingCategoryButton.classList.add('hide');
-        quizAreaContainer.classList.remove('hide');
-        categoryHeading.innerText = "Who painted it?";
-        randomQuestion = artistQuestions;
-        incrementQuestionNumber();
-        getRandomQuestion();
-        logoSign();        
+        artistQuestion();
     } else if (id === "period") {
-        artistCategoryButton.classList.add('hide');
-        periodCategoryButton.classList.add('hide');
-        paintingCategoryButton.classList.add('hide');
-        quizAreaContainer.classList.remove('hide');
-        categoryHeading.innerText = "Which period or art movement does the work belong to?";
-        randomQuestion = periodQuestions;
-        incrementQuestionNumber();
-        getRandomQuestion();
-        logoSign();
+        periodQuestion();
     } else if (id === "painting") {
-        artistCategoryButton.classList.add('hide');
-        periodCategoryButton.classList.add('hide');
-        paintingCategoryButton.classList.add('hide');
-        quizAreaContainer.classList.remove('hide');
-        categoryHeading.innerText = "Name the painting";
-        randomQuestion = paintingQuestions;
-        incrementQuestionNumber();
-        getRandomQuestion();
-        logoSign();
+        paintingQuestion();
     }
-    
+
 }
 
 // Function that replaces main Logo with one letter once inside a category game
-function logoSign(){
+function logoSign() {
     logo.classList.add("hide");
     document.getElementById("logo-letter").classList.remove("hide");
 
 }
 
-/**function to enable each category that the player chooses to play */
-// function chooseCategory(categoryType){
-//     console.log("works");
-// }
-
-/** Fhe below used https://www.youtube.com/watch?v=riDzcEQbX6k
+/** The below used https://www.youtube.com/watch?v=riDzcEQbX6k
  * as a guide and idea on how to populate questions using JS
  */
 
 /**Function that generates random questions in each category */
-function getRandomQuestion () {
+function getRandomQuestion() {
     shuffledQuestions = randomQuestion.sort(() => Math.random() - 0.5);
     // setting the index to 0 as the questions start at the first question of the array
     currentQuestionIndex = 0;
@@ -194,7 +96,7 @@ function displayQuestion(randomQuestion) {
         button.classList.add('btn');
         if (answer.correct) {
             button.id = "correct";
-        } 
+        }
 
         // create an event listener to answer buttons for when the user clicks each button then the following action follows
         button.addEventListener('click', userAnswer);
@@ -203,7 +105,7 @@ function displayQuestion(randomQuestion) {
 }
 
 /** Function that allows the user to select their answer button */
-function userAnswer(event){
+function userAnswer(event) {
     quizAreaContainer.classList.add("pointer-block");
     correctAnswer = document.getElementById("correct");
     const clickedButton = event.target;
@@ -217,7 +119,7 @@ function userAnswer(event){
 }
 
 /**Function that generates 10 questions */
-function nextQuestion(){
+function nextQuestion() {
     // check that current question index below10
     if (currentQuestionIndex < 9) {
         resetQuestion();
@@ -229,7 +131,7 @@ function nextQuestion(){
 }
 
 /** Function that resets the Question area section for the next question */
-function resetQuestion(){
+function resetQuestion() {
     nextButton.classList.add('hide');
     quizAreaContainer.classList.remove('pointer-block');
 
@@ -241,13 +143,13 @@ function resetQuestion(){
 }
 
 /** Function that shows which question the user is at against the total number of questions */
-function incrementQuestionNumber(){
+function incrementQuestionNumber() {
     questionNumberCount++;
     document.getElementById('question-number').innerHTML = questionNumberCount;
 }
 
 /** Function that controls next button */
-function handleNextButtonClick(){
+function handleNextButtonClick() {
     incrementQuestionNumber();
     nextQuestion();
 }
@@ -255,19 +157,19 @@ function handleNextButtonClick(){
 /** Function that will calculate the score as the quiz progresses. 
  * But will not display until the end 
  */
-function incrementScore(){
+function incrementScore() {
     score++;
     // score = parseInt(playerScore.innerText);
     document.getElementById("player-score").innerHTML = score;
 }
 
 /** Function that calculates the total score of correctly answered questions */
-function totalScore(){
+function totalScore() {
     totalScoreCount.innerHTML = score;
 }
 
 /**Function that generates appropriate text based on total score */
-function displayScoreText(){
+function displayScoreText() {
     if (score === 10) {
         return "You are the master of the Arts. Everyone should bow to your prowess in Art History knowledge.";
     } else if (score >= 6 && score < 10) {
@@ -280,7 +182,7 @@ function displayScoreText(){
 }
 
 /** Function that opens the Score/final page after the completion of the quiz */
-function scoreBoard(){
+function scoreBoard() {
     totalScore();
     const resultText = document.getElementById("result-description");
     resultText.textContent = displayScoreText();
@@ -293,16 +195,48 @@ function scoreBoard(){
 }
 
 /**Function that resets the game */
-function resetGame(){
+function resetGame() {
     window.location.reload();
 }
 
-/**
- * Checks the user's answer against the correct answer
- */
-// function checkCorrectAnswer(){}
+/** Function that initialises artist question category */
+function artistQuestion() {
+    artistCategoryButton.classList.add('hide');
+    periodCategoryButton.classList.add('hide');
+    paintingCategoryButton.classList.add('hide');
+    quizAreaContainer.classList.remove('hide');
+    categoryHeading.innerText = "Who painted it?";
+    randomQuestion = artistQuestions;
+    incrementQuestionNumber();
+    getRandomQuestion();
+    logoSign();
+}
 
+/** Function that initialises period question category */
+function periodQuestion() {
+    artistCategoryButton.classList.add('hide');
+    periodCategoryButton.classList.add('hide');
+    paintingCategoryButton.classList.add('hide');
+    quizAreaContainer.classList.remove('hide');
+    categoryHeading.innerText = "Which period or art movement does the work belong to?";
+    randomQuestion = periodQuestions;
+    incrementQuestionNumber();
+    getRandomQuestion();
+    logoSign();
+}
 
+/** Function that initialises painting question category*/
+function paintingQuestion() {
+    artistCategoryButton.classList.add('hide');
+    periodCategoryButton.classList.add('hide');
+    paintingCategoryButton.classList.add('hide');
+    quizAreaContainer.classList.remove('hide');
+    categoryHeading.innerText = "Name the painting";
+    randomQuestion = paintingQuestions;
+    incrementQuestionNumber();
+    getRandomQuestion();
+    logoSign();
+}
 
 
 
